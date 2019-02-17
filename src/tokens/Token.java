c96404af60b316d9fcbd367a3720af3868a23682
greg0733 <gregory.marechal@student.uliege.be>
2019-02-17 22:27:37 +0100

    { is not an RBRACE ;)

--- src/tokens/Token.java
+++ src/tokens/Token.java
@@ -29,7 +29,7 @@ public class Token{
 
         //Operators
         LBRACE("{", OPERATOR),
-        RBRACE("{", OPERATOR),
+        RBRACE("}", OPERATOR),
         LPAR("(", OPERATOR),
         RPAR(")", OPERATOR),
         COLON(":", OPERATOR),
